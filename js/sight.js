a5be432e2b0dc4ea25fa148374653cae48f6adf4
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2021-11-22 16:46:01 +0100

    correct sight class

--- js/sight.js
+++ js/sight.js
@@ -20,9 +20,9 @@ class SVGElement {
 
 class Sight {
     constructor(selector, width, height) {
-        this.avg = new SVGElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
+        this.svg = new SVGElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
     }
     draw(type, attrs) {
-        return new SVGAElement(type).attr(attrs).append(this.svg);
+        return new SVGElement(type).attr(attrs).append(this.svg);
     }
 }
