1a18e2f3d51a005281204a5ea5e84f0032d1eaf1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2021-11-22 15:39:44 +0100

    corrected namespace

--- js/sight.js
+++ js/sight.js
@@ -1,7 +1,7 @@
 class SVGElement {
     constructor(type) {
         this.type = type;
-        this.namespace - 'http://www.w3.org/2000/svg';
+        this.namespace = 'http://www.w3.org/2000/svg';
         this.node = document.createElement(this.namespace, this.type);
         return this;
     }
